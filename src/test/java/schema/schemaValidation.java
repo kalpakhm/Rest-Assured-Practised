620e44cacdd69f51030fe46fef854cf39759c737
Kalpak H M <kalpakhm@Kirans-MacBook-Pro.local>
2026-05-15 23:22:14 +0530

    added user schema validation using matchesJsonSchemaUsingClasspath

---
+++ src/test/java/schema/schemaValidation.java
@@ -0,0 +1,38 @@
+package schema;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+public class schemaValidation {
+
+	@Test
+	public void validateschema() {
+		System.out.println(getClass().getClassLoader().getResource("userschema.json"));
+
+		given()
+		
+		.when()
+		.get("https://mocki.io/v1/3d381905-170c-4e4b-ad67-e9b592c65016")
+		
+		.then()
+		.log().all()
+		.assertThat()
+		
+		// ✅ Schema validation
+		.body(matchesJsonSchemaInClasspath("userschema.json"))
+		// ✅ Basic validations
+		.body("id", equalTo(1))
+		.body("email", containsString("@"))
+		
+		// ✅ Nested validation
+		.body("address.city",equalTo("New York"))
+		
+		 // ✅ Array validation
+        .body("tags.size()", greaterThan(0))
+        .body("tags", hasItem("developer"));
+	}
+	
+}
